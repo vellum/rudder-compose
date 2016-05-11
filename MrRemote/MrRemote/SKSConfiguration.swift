fd6a282b5256136e1966dd68cb1b719b44e7defa
David Lu <radisrad@gmail.com>
2016-05-11 12:14:41 -0700

    semi functional nuance + keyboard speech recco

---
+++ MrRemote/MrRemote/SKSConfiguration.swift
@@ -0,0 +1,25 @@
+//
+//  SKSConfiguration.swift
+//  SpeechKitSample
+//
+//  All Nuance Developers configuration parameters can be set here.
+//
+//  Copyright (c) 2015 Nuance Communications. All rights reserved.
+//
+
+import Foundation
+
+var SKSAppKey = "65ffe6c6045a21228c7d0b123c57a6b7b378dead913a3d074ff0a26f7df18de2f074a8f4cec9175a46226c8279123a2c8b18d91c531623a206c61283b79f65a7"
+var SKSAppId = "NMDPTRIAL_vellumdavid_gmail_com20160509010149"
+var SKSServerHost = "sslsandbox.nmdp.nuancemobility.net"
+var SKSServerPort = "443"
+
+var SKSLanguage = "!LANGUAGE!"
+
+var SKSServerUrl = String(format: "nmsps://%@@%@:%@", SKSAppId, SKSServerHost, SKSServerPort)
+
+// Only needed if using NLU/Bolt
+var SKSNLUContextTag = "!NLU_CONTEXT_TAG!"
+
+
+let LANGUAGE = SKSLanguage == "!LANGUAGE!" ? "eng-USA" : SKSLanguage
--- MrRemote/MrRemote/ViewController.swift
+++ MrRemote/MrRemote/ViewController.swift
@@ -11,55 +11,61 @@ import UIKit
 import AVFoundation
 import AVFoundation.AVAudioSession
 import MediaPlayer
+import SpeechKit
 
-// https://stackoverflow.com/questions/24029163/finding-index-of-character-in-swift-string
-extension String {
-    public func indexOfCharacter(char: Character) -> Int? {
-        if let idx = self.characters.indexOf(char) {
-            return self.startIndex.distanceTo(idx)
-        }
-        return nil
-    }
-}
+class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegate, SKTransactionDelegate {
 
-class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegate {
+    // MARK:
+    // MARK: volume rocker
     
     var testPlayer: AVAudioPlayer? = nil
     var textView: UITextView? = nil
-    
-    // not sure i need these
     var session: AVAudioSession? = nil
     var volumeView: MPVolumeView? = nil
-    
-    // constants... where to put these?
     var maxVolume: CGFloat = 0.99999
     var minVolume: CGFloat = 0.00001
     var initialVolume: CGFloat = 0.0
     
-    // vars
+    // MARK:
+    // MARK: word selection
+
     var textIndex: Int = 0
     var words = [String]()
+
+    // MARK:
+    // MARK: speechkit
     
-    func loadSound(filename: NSString) -> AVAudioPlayer? {
-        let url = NSBundle.mainBundle().URLForResource(filename as String, withExtension: "caf")
-        if let player = try? AVAudioPlayer(contentsOfURL: url!) {
-            player.prepareToPlay()
-            return player
-        }
-        return nil
+    enum SKSState {
+        case SKSIdle
+        case SKSListening
+        case SKSProcessing
     }
+    var language: String!
+    var recognitionType: String!
+    var endpointer: SKTransactionEndOfSpeechDetection!
+    var skSession:SKSession?
+    var skTransaction:SKTransaction?
+    var state = SKSState.SKSIdle
+    var toggleRecogButton:UIButton?
+
+    // MARK:
+    // MARK: boilerplate
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // make my silent audio ambient.. this lets audio from other apps continue in background.
+        // make my "silent" audio ambient
+        // this lets audio from other apps continue in background
+        // ~ disabling this because it results in weird play/pause behavior
+        /*
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
         } catch {
             print("set avaudiosessioncategory failed")
         }
+         */
         
-        // enable volume controls by playing a dummy sample
+        // register to get volume input by playing an audio sample
         self.testPlayer = self.loadSound("silence")
         self.testPlayer?.numberOfLoops = -1
         self.testPlayer?.play()
@@ -78,40 +84,75 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegat
         self.textView?.editable = true
         self.textView?.delegate = self
         self.view.addSubview(self.textView!)
-        
         textView!.font = UIFont(name: "Helvetica", size: 24)
         textView!.contentInset = UIEdgeInsetsMake(0,0,0,0);
+        // FIXME: style this text it's awful
 
+        // speechkit
+        let mainbounds = UIScreen.mainScreen().bounds
+        self.toggleRecogButton = UIButton(frame:
+            CGRectMake(
+                mainbounds.origin.x,
+                //mainbounds.origin.y + mainbounds.size.height - 60.0,
+                mainbounds.origin.y + mainbounds.size.height/2 - 60.0,
+                mainbounds.size.width,
+                60.0
+            ))
+        self.toggleRecogButton?.backgroundColor = UIColor.lightGrayColor()
+        self.toggleRecogButton?.setTitle("Dictate", forState: .Normal)
+        self.view.addSubview(self.toggleRecogButton!)
+        self.toggleRecogButton?.addTarget(self, action: #selector(self.toggleRecognition), forControlEvents: .TouchUpInside)
+        
+        recognitionType = SKTransactionSpeechTypeDictation
+        endpointer = .Short
+        language = LANGUAGE
+        state = .SKSIdle
+        skTransaction = nil
+        skSession = SKSession(URL: NSURL(string: SKSServerUrl), appToken: SKSAppKey)
+        if (skSession == nil) {
+            let alertView = UIAlertController(title: "SpeechKit", message: "Failed to initialize SpeechKit session.", preferredStyle: .Alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
+            alertView.addAction(defaultAction)
+            presentViewController(alertView, animated: true, completion: nil)
+            return
+        }
+        loadEarcons()
+        
         
     }
     
     override func canBecomeFirstResponder() -> Bool {
         return true
     }
-    
-    override func viewWillAppear(animated: Bool) {
-        
-    }
-    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         self.becomeFirstResponder()
         UIApplication.sharedApplication().beginReceivingRemoteControlEvents()
-        
+        // don't launch the keyboard unless a touch is acquired
         self.textView?.becomeFirstResponder()
-        
-    }
-    
-    override func remoteControlReceivedWithEvent(event: UIEvent?) {
-        let rc = event!.subtype
-        print("rc.rawValue: \(rc.rawValue)")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    // MARK:
+    // MARK: intercept volume controls
     
+    func loadSound(filename: NSString) -> AVAudioPlayer? {
+        let url = NSBundle.mainBundle().URLForResource(filename as String, withExtension: "caf")
+        if let player = try? AVAudioPlayer(contentsOfURL: url!) {
+            player.prepareToPlay()
+            return player
+        }
+        return nil
+    }
+
     func listenVolumeButton(){
         let audioSession = AVAudioSession.sharedInstance()
         self.session = audioSession
@@ -123,10 +164,13 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegat
         }
     }
     
+    override func remoteControlReceivedWithEvent(event: UIEvent?) {
+        let rc = event!.subtype
+        print("rc.rawValue: \(rc.rawValue)")
+    }
+    
     override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
         if keyPath == "outputVolume"{
-            //print("got in here")
-            //print(change)
             let delta = self.initialVolume - ( change!["new"] as! CGFloat )
             if (delta != 0){
                 let num:NSNumber = NSNumber(float: Float(self.initialVolume))
@@ -138,7 +182,6 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegat
                         self.textIndex = self.words.count
                     }
                     self.selectWordAtIndex(self.textIndex)
-
                 } else if ( delta < 0 ) {
                     print("up")
                     self.textIndex -= 1
@@ -148,21 +191,12 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegat
                     self.selectWordAtIndex(self.textIndex)
                 }
             }
-            //print(change!["new"])
-            
-            // reset volume level
-            
+            // reset volume level?
         }
-        
     }
     
-    // MARK: ---
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
-    
-    // MARK: --- textviewdelegate
+    // MARK:
+    // MARK: textviewdelegate
     /*
     func textViewShouldBeginEditing(textView: UITextView) -> Bool {
         return true
@@ -180,27 +214,29 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegat
         print("did end editing")
     }
     */
+    
     func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
         return true
     }
     
     func textViewDidChange(textView: UITextView) {
-        print("did change")
-        
         self.words = (self.textView?.text.componentsSeparatedByString(" "))!
-        print(words)
-        
         self.textIndex = words.count
-        
     }
     
-    // MARK: ---
+    // MARK:
+    // MARK: text selection by volume rocker
     
     func selectAll(){
         textView?.selectedTextRange = textView?.textRangeFromPosition((textView?.beginningOfDocument)!, toPosition: (textView?.endOfDocument)!)
     }
     
     func selectWordAtIndex(index:Int){
+        
+        if (words.count == 0){
+            return
+        }
+        
         if (index < 0){
             let pos0 = textView?.beginningOfDocument
             let pos1 = pos0
@@ -229,6 +265,159 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, UITextViewDelegat
         textView?.selectedTextRange = textView?.textRangeFromPosition(pos0!, toPosition: pos1!)
     }
     
+    func insertText(message:String) {
+        //print("inserting text: ")
+        //print(message)
+        var selectedRange: NSRange = (self.textView?.selectedRange)!
+        print(selectedRange)
+        self.textView!.text = self.textView?.text.stringByAppendingString(" ")
+        self.textView!.text = self.textView?.text.stringByAppendingString(message)
+        
+        
+        //self.becomeFirstResponder()
+        
+        /*
+        // disable hud (this replaces the default hud with a custom 0x0 view
+        self.volumeView = MPVolumeView(frame: CGRectMake(-1000,-1000,0,0) )
+        UIApplication.sharedApplication().windows.first?.addSubview(self.volumeView!)
+        listenVolumeButton()
+        */
+        
+        self.textViewDidChange(self.textView!)
+    }
+
+    // MARK:
+    // MARK: speechkit - helpers
+    
+    func log(message: String) {
+        //logTextView!.text = logTextView!.text.stringByAppendingFormat("%@\n", message)
+        print(message)
+    }
+    
+    func resetTransaction() {
+        NSOperationQueue.mainQueue().addOperationWithBlock({
+            self.skTransaction = nil
+            //self.toggleRecogButton?.setTitle("recognizeWithType", forState: .Normal)
+            //self.toggleRecogButton?.enabled = true
+        })
+    }
+    
+    func loadEarcons() {
+        let startEarconPath = NSBundle.mainBundle().pathForResource("sk_start", ofType: "pcm")
+        let stopEarconPath = NSBundle.mainBundle().pathForResource("sk_stop", ofType: "pcm")
+        let errorEarconPath = NSBundle.mainBundle().pathForResource("sk_error", ofType: "pcm")
+        let audioFormat = SKPCMFormat()
+        audioFormat.sampleFormat = .SignedLinear16
+        audioFormat.sampleRate = 16000
+        audioFormat.channels = 1
+        
+        skSession!.startEarcon = SKAudioFile(URL: NSURL(fileURLWithPath: startEarconPath!), pcmFormat: audioFormat)
+        skSession!.endEarcon = SKAudioFile(URL: NSURL(fileURLWithPath: stopEarconPath!), pcmFormat: audioFormat)
+        skSession!.errorEarcon = SKAudioFile(URL: NSURL(fileURLWithPath: errorEarconPath!), pcmFormat: audioFormat)
+    }
+
+    /*
+     func startPollingVolume() {
+     // Every 50 milliseconds we should update the volume meter in our UI.
+     volumePollTimer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: #selector(SKSASRViewController.pollVolume), userInfo: nil, repeats: true)
+     }
+     
+     func pollVolume() {
+     let volumeLevel = skTransaction!.audioLevel
+     volumeLevelProgressView!.setProgress(volumeLevel / Float(100), animated: true)
+     }
+     
+     func stopPollingVolume() {
+     volumePollTimer!.invalidate()
+     volumePollTimer = nil
+     volumeLevelProgressView!.setProgress(Float(0), animated: true)
+     }
+     */
+    // MARK: - ASR Actions
+    func toggleRecognition() {
+        switch state {
+        case .SKSIdle:
+            recognize()
+        case .SKSListening:
+            stopRecording()
+        case .SKSProcessing:
+            cancel()
+        }
+    }
+    
+    func recognize() {
+        // Start listening to the user.
+        toggleRecogButton?.setTitle("Stop", forState: .Normal)
+        skTransaction = skSession!.recognizeWithType(recognitionType,
+                                                     detection: endpointer,
+                                                     language: language,
+                                                     delegate: self)
+    }
+    
+    func stopRecording() {
+        // Stop recording the user.
+        skTransaction!.stopRecording()
+        
+        // Disable the button until we received notification that the transaction is completed.
+        toggleRecogButton?.enabled = false
+    }
+    
+    func cancel() {
+        // Cancel the Reco transaction.
+        // This will only cancel if we have not received a response from the server yet.
+        skTransaction!.cancel()
+    }
+    
+    // MARK: - SKTransactionDelegate
+    
+    func transactionDidBeginRecording(transaction: SKTransaction!) {
+        log("transactionDidBeginRecording")
+        
+        state = .SKSListening
+        //startPollingVolume()
+        toggleRecogButton?.setTitle("Listening..", forState: .Normal)
+    }
+    
+    func transactionDidFinishRecording(transaction: SKTransaction!) {
+        log("transactionDidFinishRecording")
+        
+        state = .SKSProcessing
+        //stopPollingVolume()
+        toggleRecogButton?.setTitle("Processing..", forState: .Normal)
+    }
+    
+    func transaction(transaction: SKTransaction!, didReceiveRecognition recognition: SKRecognition!) {
+        log(String(format: "didReceiveRecognition: %@", arguments: [recognition.text]))
+        
+        state = .SKSIdle
+        
+        self.insertText(recognition.text)
+        
+        toggleRecogButton?.setTitle("Dictate", forState: .Normal)
+        toggleRecogButton?.enabled = true
+
+    }
+    
+    func transaction(transaction: SKTransaction!, didReceiveServiceResponse response: [NSObject : AnyObject]!) {
+        log(String(format: "didReceiveServiceResponse: %@", arguments: [response]))
+    }
+    
+    func transaction(transaction: SKTransaction!, didFinishWithSuggestion suggestion: String) {
+        log("didFinishWithSuggestion")
+        
+        state = .SKSIdle
+        resetTransaction()
+    }
+    
+    func transaction(transaction: SKTransaction!, didFailWithError error: NSError!, suggestion: String) {
+        log(String(format: "didFailWithError: %@. %@", arguments: [error.description, suggestion]))
+        
+        // Something went wrong. Ensure that your credentials are correct.
+        // The user could also be offline, so be sure to handle this case appropriately.
+        
+        state = .SKSIdle
+        resetTransaction()
+    }
     
 }
 
